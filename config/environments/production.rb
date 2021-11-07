4c9befb74feec8344d7ffb2a89d2b224d55b7d23
tatchan <43107628+TATCHAN0825@users.noreply.github.com>
2021-11-07 19:34:35 +0900

    メールの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,6 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
+  config.action_mailer.default_url_options = { host: 'https://bingo-game-tatchan.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
