66c16da0ba679533df76a7b15c6268e88e2f516e
tatchan <43107628+TATCHAN0825@users.noreply.github.com>
2021-11-07 19:50:18 +0900

    boardsをrootにする

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'boards#index'
   resources :boards
   post 'boards/:id/join' => 'boards#join', as: :boards_join
   get 'profile/:name' => 'profile#show'
